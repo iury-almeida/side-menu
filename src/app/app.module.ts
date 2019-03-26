277cf61807502ad9df35d139136d3ff8f1f58529
iury-almeida <iury@smn.com.br>
2019-03-26 10:32:43 -0300

    side-menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './side-menu/navigation/navigation.component';
 import { SecondComponent } from './second/second.component';
 import { FirstComponent } from './first/first.component';
 import {RouterModule, Routes} from "@angular/router";
@@ -11,7 +11,7 @@ import {CustomMaterialModule} from "./core/material.module";
 import { LoginLayoutComponent } from './login-layout/login-layout.component';
 import { HomeLayoutComponent } from './home-layout/home-layout.component';
 import { LoginComponent } from './login/login.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ToolbarComponent } from './side-menu/toolbar/toolbar.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'login', data: { title: 'First Component' }, pathMatch: 'full' },
